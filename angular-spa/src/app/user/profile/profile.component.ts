cd6bbf0264b0b6edc1d9884c21c945057a57a6b8
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-07-31 20:12:09 +0300

    implement user Info on profile page

--- angular-spa/src/app/user/profile/profile.component.ts
+++ angular-spa/src/app/user/profile/profile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from 'src/app/shared/types/user';
+import { Record } from 'src/app/shared/types/record';
+import { RecordService } from 'src/app/records/record.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+   user: User | undefined = undefined;
+  
+
+  constructor(private userService: UserService, private recordService: RecordService){}
+
+ngOnInit(): void {
+  const user = this.userService.getUser();
+  this.user = user;
+  
+
+}
+
+
 
 }
