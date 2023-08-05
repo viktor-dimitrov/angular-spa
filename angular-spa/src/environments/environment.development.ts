9a32f5e10de3a8b57bd4c0f8ac6cbe39fda77c42
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-08-05 19:13:46 +0300

    change URL

--- angular-spa/src/environments/environment.development.ts
+++ angular-spa/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    // dataUrl: 'https://panicky-undershirt-mite.cyclic.app/data',
-    // authUrl: 'https://panicky-undershirt-mite.cyclic.app/users',
-    dataUrl: 'http://localhost:3000/data',
-    authUrl: 'http://localhost:3000/users'
+    dataUrl: 'https://panicky-undershirt-mite.cyclic.app/data',
+    authUrl: 'https://panicky-undershirt-mite.cyclic.app/users',
+    // dataUrl: 'http://localhost:3000/data',
+    // authUrl: 'http://localhost:3000/users'
 };
