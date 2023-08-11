93027b6ec66003d03c4b38f80847335a3ef5f23a
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-08-11 21:48:51 +0300

    add stats component

---
+++ angular-spa/src/app/shared/stats/stats.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.css']
+})
+export class StatsComponent {}
