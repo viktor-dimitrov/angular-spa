cf1f06b245e39009652e977725ae83752e600409
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-08-11 22:09:35 +0300

    deploy

--- angular-spa/src/environments/environment.development.ts
+++ angular-spa/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // apiUrl: 'https://panicky-undershirt-mite.cyclic.app',
-   apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://panicky-undershirt-mite.cyclic.app',
+    //apiUrl: 'http://localhost:3000',
 };
